8fa0f87c8cad1a7069dd775d419cdab47e5eced7
Mary <l.skyling@gmail.com>
2017-09-07 14:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function add(x, y) {
   return x + y;
 }
+
 function subtract(x, y) {
   return x - y;
 }
+
 function multiply(a, b) {
   return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
 }
